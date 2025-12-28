c2f8172bf09cd404ed3a4c3a8e22ca0421aa57c0
Renzi Fidele <renzifidele2001@gmail.com>
2025-12-28 20:54:22 +0200

    feat: Implementando o botão de mudança de idioma no cabeçalho  do mobile

--- src/Components/HeaderMobile/HeaderMobile.jsx
+++ src/Components/HeaderMobile/HeaderMobile.jsx
@@ -1,15 +1,33 @@
 import styles from "./HeaderMobile.module.css";
 import logo from "../../Images/lg.png";
 import { NavValue } from "../../Context/NavContext";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaGlobe } from "react-icons/fa";
+import { LangValue } from "../../Context/LangContext";
+import i18n from "../../i18n/i18n";
 
 const HeaderMobile = ({ onLogoClick }) => {
    //  Contexto da nav
    const navAtivo = NavValue();
+   const { idioma, setIdioma } = LangValue();
+
+   i18n.on("languageChanged", (lng) => setIdioma(lng));
+   function mudarIdioma(idioma) {
+      i18n.changeLanguage(idioma);
+   }
 
    return (
       <header id={styles.header}>
          <img src={logo} alt="logo " onClick={onLogoClick} />
+         <div className={styles.langCt}>
+            <FaGlobe />
+            <p onClick={() => mudarIdioma("en")} className={idioma?.includes("en") ? styles.ativo : ""}>
+               EN
+            </p>
+            /
+            <p onClick={() => mudarIdioma("pt")} className={idioma?.includes("pt") ? styles.ativo : ""}>
+               PT
+            </p>
+         </div>
          <span
             onClick={() => {
                navAtivo.setNav(!navAtivo.nav);
